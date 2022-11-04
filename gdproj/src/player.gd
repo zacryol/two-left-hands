d70ddf6fd5eb53f5c8e15470e6eb86125310b755
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2022-11-04 12:33:39 -0600

    use statemachine instead

--- gdproj/src/player.gd
+++ gdproj/src/player.gd
@@ -1,8 +1,14 @@
 class_name Player
 extends CharacterBody2D
 
+## Movement Speed.
 @export var speed: float = 150.0
-@onready var anim_tree := $AnimationTree as AnimationTree
+
+@onready var _anim_tree := $AnimationTree as AnimationTree
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("action1"):
+		pass
 
 func _physics_process(_delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -11,5 +17,6 @@ func _physics_process(_delta: float) -> void:
 	var _r := move_and_slide()
 
 
+## Sets the blend position used for movement direction animations
 func set_blend_position(pos: Vector2):
-	anim_tree.set("parameters/BlendSpace2D/blend_position", pos)
+	_anim_tree.set("parameters/Standard/blend_position", pos)
