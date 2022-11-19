34a6dd4cf873eb408092f20c5561d4666d22800b
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2022-11-19 10:43:05 -0700

    added enemy spawner to arena

--- gdproj/src/enemy.gd
+++ gdproj/src/enemy.gd
@@ -1,19 +1,23 @@
+class_name Enemy
 extends Area2D
-
 
 @onready var player := get_tree().get_first_node_in_group(&"player") as Player
 @onready var sprite := $Sprite2D as Sprite2D
 
+## Base Speed, modified by aggression for actual speed
 @export var base_speed := 65.0
 
+## How fast this one moves
+## Final speed is [code](aggression + 0.5) * base_speed[/code]
 var aggression := 0.5
 
 func _ready() -> void:
-	modulate = Color.WHITE.lerp(Color.RED, aggression)
+	var redness := maxf(remap(aggression, 0.5, 1, 0, 1), 0)
+	modulate = Color.WHITE.lerp(Color.RED, redness)
 
 
 func _physics_process(delta: float) -> void:
 	if not player: return
 	global_position = global_position.move_toward(player.global_position, 
-			base_speed * aggression * delta)
+			base_speed * (aggression + 0.5) * delta)
 	sprite.flip_h = player.global_position.x < global_position.x
---
+++ gdproj/src/spawner.gd
@@ -0,0 +1,26 @@
+class_name EnemySpawner
+extends PathFollow2D
+
+const _SPAWN_THRESHOLD := 100.0
+var spawn_rate := 20.0
+
+var EnemyScene = preload("res://src/enemy.tscn") as PackedScene
+
+var _spawn_tick := 0.0
+
+func _init() -> void:
+	randomize()
+
+func _process(delta: float) -> void:
+	_spawn_tick += spawn_rate * delta
+	while _spawn_tick > _SPAWN_THRESHOLD:
+		_spawn_tick -= _SPAWN_THRESHOLD
+		spawn_enemy()
+
+## Spawn enemy at random point along path
+func spawn_enemy() -> void:
+	progress_ratio = randf()
+	var enemy := EnemyScene.instantiate() as Enemy
+	enemy.aggression = randf() * randf()
+	(owner if owner else get_parent()).add_child(enemy)
+	enemy.global_position = global_position
