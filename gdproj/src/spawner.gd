b58b4529dd8cdf0600070076e24beb8dd963e519
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2022-11-23 15:52:26 -0700

    remove rng in potion spawns

--- gdproj/src/spawner.gd
+++ gdproj/src/spawner.gd
@@ -2,14 +2,18 @@ class_name EnemySpawner
 extends PathFollow2D
 
 const _SPAWN_THRESHOLD := 100.0
-var spawn_rate := 20.0
+var spawn_rate := 200.0
 
 var EnemyScene = preload("res://src/enemy.tscn") as PackedScene
 
 var _spawn_tick := 0.0
 
+var _enemies_spawned := 0
+var _spawn_potion_at := 8
+
 func _init() -> void:
 	randomize()
+
 
 func _process(delta: float) -> void:
 	spawn_rate = minf(spawn_rate, 6000.0)
@@ -18,6 +22,7 @@ func _process(delta: float) -> void:
 			get_tree().get_nodes_in_group(&"enemy").size() < 500:
 		_spawn_tick -= _SPAWN_THRESHOLD
 		spawn_enemy()
+
 
 ## Spawn enemy at random point along path
 func spawn_enemy() -> void:
@@ -28,7 +33,9 @@ func spawn_enemy() -> void:
 	(owner if owner else get_parent()).add_child(enemy)
 	enemy.global_position = global_position
 	var _err := enemy.died.connect(Callable(self, &"_enemy_died"))
-	if randf() < 2.0 / spawn_rate:
+	_enemies_spawned += 1
+	if _enemies_spawned > _spawn_potion_at:
+		_spawn_potion_at *= 2
 		enemy.has_potion = true
 
 
